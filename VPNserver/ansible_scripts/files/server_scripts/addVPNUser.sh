8d5a41ab4db1ab982f7ced9c2eabd9ef70daaf1d
Sirjeleshka <sirjeleshka@gmail.com>
2021-11-30 23:24:54 +0200

    Removed old setup VPN server data

--- VPNserver/ansible_scripts/files/server_scripts/addVPNUser.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-[ -z $1 ] && echo "Use $0 clientname" && exit 0
-
-clientName=${1}
-
-docker run -v vpn_data:/etc/openvpn --log-driver=none --rm -it kylemanna/openvpn easyrsa build-client-full ${clientName} nopass &&
-    docker run -v vpn_data:/etc/openvpn --log-driver=none --rm kylemanna/openvpn ovpn_getclient ${clientName} > ${clientName}.ovpn
