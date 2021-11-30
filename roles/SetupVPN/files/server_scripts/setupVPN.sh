371304a98efa3d8092f8b0e7ecd1c7b3d5b94388
Sirjeleshka <sirjeleshka@gmail.com>
2021-11-30 23:05:48 +0200

    Added script of setup VPN server

---
+++ roles/SetupVPN/files/server_scripts/setupVPN.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+    ip=`curl ifconfig.me` &&
+    docker volume create --name vpn_data &&
+    docker run -v vpn_data:/etc/openvpn --rm kylemanna/openvpn ovpn_genconfig -u ${ip} &&
+    docker run -v vpn_data:/etc/openvpn --log-driver=none --rm -it kylemanna/openvpn ovpn_initpki &&
+    docker run -v vpn_data:/etc/openvpn -d -p 1194:1194/udp --cap-add=NET_ADMIN kylemanna/openvpn
