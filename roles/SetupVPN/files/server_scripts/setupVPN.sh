97ec2c97851439e6c57629b85ace8e6f14f599ca
Sirjeleshka <sirjeleshka@gmail.com>
2022-02-14 13:24:32 +0200

    Added verbose configuration file

--- roles/SetupVPN/files/server_scripts/setupVPN.sh
+++ roles/SetupVPN/files/server_scripts/setupVPN.sh
@@ -4,4 +4,4 @@
     docker volume create --name vpn_data &&
     docker run -v vpn_data:/etc/openvpn --rm kylemanna/openvpn ovpn_genconfig -u ${ip} &&
     docker run -v vpn_data:/etc/openvpn --log-driver=none --rm -it kylemanna/openvpn ovpn_initpki &&
-    docker run -v vpn_data:/etc/openvpn -d -p 1194:1194/udp --cap-add=NET_ADMIN kylemanna/openvpn
+    docker run -v vpn_data:/etc/openvpn -v ~/scripts/openvpn.conf:/etc/openvpn/openvpn.conf -d -p 1194:1194/udp --cap-add=NET_ADMIN kylemanna/openvpn
